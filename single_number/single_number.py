a9c00ccea8094a7a119d6f4b29aafd436d788bb5
jesseleeruiz <jesseleeruiz@gmail.com>
2020-07-22 10:04:36 -1000

    Completed single_number. Test passes.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,11 +2,26 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+'''
+PLAN
+    # Loop through the array
+    # Mark the first number to compare to other numbers
+    # If the same one comes around then remove those numbers
+    # Remove all the numbers until the odd one out is left.
+'''
 def single_number(arr):
     # Your code here
-
-    pass
-
+    current = arr[0]
+    for number in arr[1:]:
+        if current == number:
+            arr.remove(current)
+            arr.remove(number)
+            single_number(arr)
+            return arr[0]
+            break
+        # else:
+        #     return arr[0]
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
