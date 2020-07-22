5d73b0dfe43732fe476a1d1fa3fe4d13dbe1156f
jesseleeruiz <jesseleeruiz@gmail.com>
2020-07-22 10:15:16 -1000

    Completed moving_zeroes. Test passes.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,20 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+'''
+PLAN
+    # Loop through array
+    # If number == 0
+        # Remove the zero and extend the array with 0
+'''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    for number in arr:
+        if number == 0:
+            arr.remove(number)
+            arr.append(number)
+    return arr
 
 
 if __name__ == '__main__':
